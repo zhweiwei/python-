b107638d668ef35058438bc136dd5d9d0d23a365
zhweiwei <1239218305@qq.com>
2020-04-08 11:12:39 +0800

    字符串处理

--- 字符串处理.py
+++ 字符串处理.py
@@ -1,11 +1,23 @@
 import string
 
-s = " sfos sos svo "
+# s = " sfos sos svo "
+#
+# print(s)
+# print(s.strip()) #处理左右空格
+# print(s.lstrip()) #处理左边空格 lstrip = left strip
+# print(s.rstrip()) #处理右边空格 rstrip =  right strip
 
-print(s)
-print(s.strip()) #处理左右空格
-print(s.lstrip()) #处理左边空格 lstrip = left strip
-print(s.rstrip()) #处理右边空格 rstrip =  right strip
+# s1 = "abb abb"
+# s2 = "ABC"
+#
+# print(s1.upper())
+# print(s2.lower())
+# print(s1.capitalize())
 
 
-
+# s = "sbvsusn"
+# print(s.index("vs"))
+# try:
+#     print(s.index("vsa"))
+# except ValueError:
+#     pass
