b8b34494c92953d03cdf93cc6be85edc3a5f52aa
zhweiwei <1239218305@qq.com>
2020-04-09 20:56:55 +0800

    函数

--- 函数.py
+++ 函数.py
@@ -25,12 +25,12 @@
 #
 # fun(12,*(12,32,43,32))
 
-def fun(arg1, **arg2):
-    print(type(arg2))
-    print(arg1)
-    print(arg2)
-
-fun(12,**{"name" :"zhang","value ": "sfgi"})
+# def fun(arg1, **arg2):
+#     print(type(arg2))
+#     print(arg1)
+#     print(arg2)
+#
+# fun(12,**{"name" :"zhang","value ": "sfgi"})
 
 # def fun(arg1,*,arg2):
 #
@@ -38,3 +38,20 @@ fun(12,**{"name" :"zhang","value ": "sfgi"})
 #     print(arg2)
 #
 # fun(1, arg2 = 2)
+
+def fun(arg1, arg2, cmp = None):
+    if cmp is None:
+        if arg1 > arg2:
+            return 1
+        elif arg1 == arg2:
+            return 0
+        else:
+            return -1
+    else:
+        return cmp(arg1,arg2)
+
+def cmp(arg1, arg2):
+    return "this is cmp"
+
+print(fun(1,2))
+print(fun(1,2,cmp))
