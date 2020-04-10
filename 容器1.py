246f23deabb8029955640ada6fb2b4e018123c4e
zhweiwei <1239218305@qq.com>
2020-04-10 10:20:17 +0800

    容器

---
+++ 容器1.py
@@ -0,0 +1,49 @@
+l = [1, 2, 3]
+t = (1, 3, 4)
+
+# s = [12, 3, 43, 5]
+# print(s[1])
+
+# s = [12, 3, 43, 5]
+# print(s.index(3))
+# print(s.index(33))
+
+
+# s = [12, 3, 43, 5]
+# s1 = ["dfb", "insi"]
+# s.append(88)
+# print(s)
+# s.extend(s1)
+# print(s)
+
+# s = [12, 3, 43, 5]
+# print(s)
+# del(s[2])
+# print(s)
+
+# s = [12, 3, 43, 5]
+#
+# for value in s:
+#     print(value)
+
+# d = {"sds":"sfvs"}
+# print(type(d.items()))
+
+# d = {"a": "dfvbo", 2: "sfvno", "fv": 31}
+# for index in d:
+#     print(d[index])
+#
+# for index, value in d.items():
+#     print(str(index) + " : " + str(value))
+
+# d = {"a": "dfvbo", 2: "sfvno", "fv": 31}
+#
+# if 2 in d:
+#     print(d[2])
+# else:
+#     print("2 not in d")
+
+# d = {"a": "dfvbo", 2: "sfvno", "fv": 31}
+# print(d["a"])
+# d["a"] = "net value"
+# print(d["a"])
