4d23377f1c89e28869d35b86ce5ded234a87273d
zhweiwei <1239218305@qq.com>
2020-04-10 08:14:44 +0800

    容器

---
+++ 容器.py
@@ -0,0 +1,19 @@
+# s = [12, 3, 43, 5]
+# print(s[1])
+
+# s = [12, 3, 43, 5]
+# print(s.index(3))
+# print(s.index(33))
+
+
+# s = [12, 3, 43, 5]
+# s1 = ["dfb", "insi"]
+# s.append(88)
+# print(s)
+# s.extend(s1)
+# print(s)
+
+# s = [12, 3, 43, 5]
+# print(s)
+# del(s[2])
+# print(s)
