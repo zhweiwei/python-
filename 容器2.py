646359eccee1fcc45833ae74edb463e564a4c8d4
zhweiwei <1239218305@qq.com>
2020-04-10 11:07:54 +0800

    容器

---
+++ 容器2.py
@@ -0,0 +1,34 @@
+# s = set((1, 2, 3, 2, 2, 4))
+# print(s)
+
+# s1 = set([1,2,3,4,5,6])
+# s2 = set([3,4,5,6,7,8,9])
+# print(s1 & s2)
+# print(s1.intersection(s2))
+
+# s1 = set([1, 2, 3, 4, 5, 6])
+# s2 = set([3, 4, 5, 6, 7, 8, 9])
+# print(s1 - s2)
+# print(s1.difference(s2))
+
+# s1 = set([1, 2, 3, 4, 5, 6])
+# s2 = set([3, 4, 5, 6, 7, 8, 9])
+# print(s1 ^ s2)
+# print(s1.symmetric_difference(s2))
+#
+# s = set([1, 2])
+# print(s)
+# s.add(3)
+# print(s)
+# s.update(set([5, 6, 7]))
+# print(s)
+#
+# s = set([1, 2, 3, 4, 5, 6])
+# print(s)
+# s.remove(4)
+#
+# print(s)
+
+s = set([1, 2, 3, 4, 5, 6])
+for value in s:
+    print(value)
